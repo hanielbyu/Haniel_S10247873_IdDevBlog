04198cfd0b9230a287ab70861685f18c3d2c72e0
Haniel Yu <s10247873@connect.np.edu.sg>
2023-02-15 20:28:01 +0800

    ID Developement Blog

---
+++ script.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation ({
+    container: document.getElementById('animContainer'),
+    renderer : 'svg',
+    loop: true,
+    autoplay : true,
+    path: 'https://assets3.lottiefiles.com/packages/lf20_lS88YC8r3Y.json'
+
+})
